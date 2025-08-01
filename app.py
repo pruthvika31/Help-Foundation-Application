fc2fd3cc1c94485cf5301c7fc21895718d9f9c25
pruthvika31 <92929258+pruthvika31@users.noreply.github.com>
2025-08-01 12:00:40 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,51 @@
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+import joblib
+
+# first lets load the instances that were created
+
+with open('scaler.joblib','rb') as file:
+    scale=joblib.load(file)
+
+with open('pca.joblib','rb') as file:
+    pca=joblib.load(file)
+
+with open('final_model.joblib','rb'):
+    model=joblib.load(file)
+
+def prediction(input_list):
+    scaled_input=scale.transform([input_list])
+    pca_input=pca.transform(scaled_input)
+    model.predict(pca_input)[0]
+
+    if output==0:
+        return 'Developed'
+    elif output==1:
+        return 'Underdeveloped'
+    else:
+        return 'Developing'
+
+def main():
+    st.title('HELP NGO FOUNDATION')
+    st.subheader('This application will give the status of a country based on socio-economic and health factors.')
+    gdp=st.text_input('Enter the GDP per population of a country')
+    inc=st.text_input('Enter the per capita income of a country')
+    imp=st.text_input('Enter the imports in terms of percentage of GDP')
+    exp=st.text_input('Enter the exports in terms of percentage of GDP')
+    inf=st.text_input('Enter the inflation rate in a country (%)')
+
+    hel=st.text_input('Enter the health expenditure in terms of % of GDP')
+    ch_m=st.text_input('Enter the no. of deaths per 100 births for <5 years')
+    fer=st.text_input('Enter the avg children born to a each women in a country')
+    lf=st.text_input('Enter the avg life expectancy in a country')
+
+    in_data=[ch_m,exp,hel,imp,inc,inf,lf,fer,gdp]
+
+    if st.button('Predict'):
+        response=prediction(in_data)
+        st.success(response)
+
+if __name__=='__main__':
+    main()
